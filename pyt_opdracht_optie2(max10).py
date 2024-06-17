577e231e3c8c02929407cc2d6bf64891341ddaa5
Dexter <166765537+USA-DeepDarkFantasy@users.noreply.github.com>
2024-06-17 04:17:55 +0200

    Update pyt_opdracht_optie2(max10).py

--- pyt_opdracht_optie2(max10).py
+++ pyt_opdracht_optie2(max10).py
@@ -9,11 +9,10 @@
 #Type 4 voor verwijderen van een model bij een merk 
 #Type 0 voor afsluiten 
 
-automerk_lijst = {
-1 : {Audi:['A4','A6']}, 
-2 : {BMW:['3 serie', '5 serie']},
-3 : {Mercedes:['E klasse','S klasse']},
-4 : {Porsche:['718','911']}
+x = automexrk_lijst = { 1 : {'Audi':['A4','A6']}, 
+2 : {'BMW':['3 serie', '5 serie']},
+3 : {'Mercedes':['E klasse','S klasse']},
+4 : {'Porsche':['718','911']}
 }
 #list in the list, feels like a pro 
 
@@ -25,43 +24,62 @@ while True:
     print('Type 3 voor toevoegen van een model bij een merk')
     print('Type 4 voor verwijderen van een model bij een merk')
     print('Type 0 voor afsuluiten')
+    print('Type 5 voor het printen van de tegenwoordig lijst')
     #asking the choice, ask to the input a number 
     keuze = input('jouw keuze:')
     #fill in the number if it is equal to 0,1,2,3,4
 
     if keuze == "1":
         automerk = input("Voer het automerk die je wilt toevoegen:")
-        #asking to input the car that user wanna add
-        uitgebreid_lijst = len(automerk_lijst) + 1
-         #add the new brand to the list, old code is not logical, becoz it is list in the list. append might go first, but not sure.
-        #need to learn 'len'.
-        automerk_lijst[uitgebreid_lijst] =             
-             
-             
-       
-
-        
-        print(f"{automerk} is toegevoed in de lijst")
+        # asking to input the car that user wanna add
+        x[len(x) + 1] = {automerk: []}
+        # add the new brand to the list
+        print(f"{automerk} is toegevoegd aan de lijst")
         print(" ")
-        #print the freedback, and print an empty line
+        # print the feedback, and print an empty line
 
  # if is 2, then add for the car brand
     elif keuze == "2":
+        #####bug here, I typed 1 and brand is lambo and type 5 to check the new list, lambo is in the list, but 2 reflects lambo not in the list.####
         automerk = input("Voer het automerk die je wilt verwijderen:")
         #asking to input the car that user wanna delete
-        if automerk in automerk_lijst:
+        if automerk in (x):
         #got warning, i  need to add  a conditon  if the brand is already in the list. If not, print we dont have this car brand.
-            automerk_lijst.remove(automerk)
+            del x[automerk]
         #remove the brand from the list
             print(f"{automerk} is verwijderd van de lijst.")
             print("")
-            #print the feedback that it is deleted.
+            #print the feedback the car brand it is deleted.
         else:
                 print(f"{automerk} is niet in de lijst.")
                 #print we dont have this car brand.
                 print("Nogweer invoeren op de menu.")
                 #print asking again with menu.
                 print(" ")
+                
+    elif keuze == "3":
+        automerk = input("Voer het automerk die je wilt toevoegen:")
+        model = input("Voer het model die je wilt toevoegen:")
+        #asking which car that user wanna add model, first ask the brand, then ask the model
+        if automerk in x:
+            x([automerk].append(model))
+            automerk[len(automerk) + 1] = {model: []}
+        #add the new model to the list
+            print(f"{model} is toegevoegd aan de lijst")
+            print(" ")
+            #print the feedback, and print an empty line
+        else:
+            print(f"{automerk} is niet in de lijst.")
+            #print this car brand is not in our list. he needs to add the car brand first.
+            print("Nogweer invoeren op de menu.")
+            #print asking again with menu.
+            print(" ")
+
+    elif keuze == "5":
+        print(x)
+        print(" ")
+        #print the list of the car brand and model.
+
   # if input is 0, then kill the script.
     elif keuze == "0":
          print("Dit programa sluit af. Bedankt voor het gebruiken.")
@@ -75,4 +93,3 @@ while True:
          print(" ")
 
 # if it is outside the option, ask again.
-
